32be01cf926cdad54472839e6534f5b46ac62a2b
BunnyNabbit (Aon) <aon@bunnynabbit.com>
2025-07-02 05:17:15 -0500

    Move DerivedModelGraph class to file

---
+++ class/DerivedModelGraph.cjs
@@ -0,0 +1,30 @@
+class DerivedModelGraph {
+	/** */
+	static traverse(instance, set) {
+		if (instance.SourceAssetId && instance.SourceAssetId !== "-1") {
+			set.add(instance.SourceAssetId)
+		}
+
+		if (instance.Children) {
+			for (const child of instance.Children) {
+				DerivedModelGraph.traverse(child, set)
+			}
+		}
+	}
+
+	static scanModel(modelPazh) {
+		const buffer = fs.readFileSync(modelPazh)
+		const { result } = reader.parseBuffer(buffer)
+		const sourceAssetIds = new Set()
+
+		result.forEach((instance) => {
+			this.traverse(instance, sourceAssetIds)
+		})
+
+		return sourceAssetIds
+	}
+}
+
+module.exports = {
+	DerivedModelGraph,
+}
--- modera.cjs
+++ modera.cjs
@@ -1,35 +1,5 @@
 const fs = require("node:fs")
 const reader = require("rbx-reader")
-
-class DerivedModelGraph {
-	/** */
-	static traverse(instance, set) {
-		if (instance.SourceAssetId && instance.SourceAssetId !== "-1") {
-			set.add(instance.SourceAssetId)
-		}
-
-		if (instance.Children) {
-			for (const child of instance.Children) {
-				DerivedModelGraph.traverse(child, set)
-			}
-		}
-	}
-
-	static scanModel(modelPazh) {
-		const buffer = fs.readFileSync(modelPazh)
-		const { result } = reader.parseBuffer(buffer)
-		const sourceAssetIds = new Set()
-
-		result.forEach((instance) => {
-			this.traverse(instance, sourceAssetIds)
-		})
-
-		return sourceAssetIds
-	}
-}
+const { DerivedModelGraph } = require("./class/DerivedModelGraph.cjs")
 
 console.log(DerivedModelGraph.scanModel("./model.rbxm"))
-
-module.exports = {
-	DerivedModelGraph,
-}
