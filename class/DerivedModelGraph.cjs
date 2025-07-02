dc493b80622726fa0f8729e58f9d23363192f887
BunnyNabbit (Aon) <aon@bunnynabbit.com>
2025-07-02 05:32:37 -0500

    Add JSDoc comments in DerivedModelGraph

--- class/DerivedModelGraph.cjs
+++ class/DerivedModelGraph.cjs
@@ -1,8 +1,12 @@
 const fs = require("node:fs")
 const reader = require("rbx-reader")
+/** @typedef {import("rbx-reader/dist/Instance").Instance} Instance */
 
 class DerivedModelGraph {
-	/** */
+	/**Recursively traverses the model instance tree and collects SourceAssetIds.
+	 * @param {Instance} instance - An instance to traverse.
+	 * @param {Set} set - The set to collect SourceAssetIds.
+	 */
 	static traverse(instance, set) {
 		if (instance.SourceAssetId && instance.SourceAssetId !== "-1") {
 			set.add(instance.SourceAssetId)
@@ -14,7 +18,10 @@ class DerivedModelGraph {
 			}
 		}
 	}
-
+	/**Scans a model file for SourceAssetIds.
+	 * @param {string} modelPazh - The path to the model file.
+	 * @returns {Set} A set of SourceAssetIds found in the model.
+	 */
 	static scanModel(modelPazh) {
 		const buffer = fs.readFileSync(modelPazh)
 		const { result } = reader.parseBuffer(buffer)
@@ -26,7 +33,10 @@ class DerivedModelGraph {
 
 		return sourceAssetIds
 	}
-
+	/**Scans a directory for model files and collects any unique instances of SourceAssetId.
+	 * @param {string} directoryPath - The path to the directory containing model files.
+	 * @returns {Map} A map where keys are model file names and values are sets of SourceAssetIds.
+	 */
 	static scanDirectory(directoryPath) {
 		const modelSets = new Map()
 
